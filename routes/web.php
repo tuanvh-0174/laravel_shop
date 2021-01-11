c0d2ce91b56221488fab7bd2466dcda9c6da0761
vu.huy.tuan <SUN-ASTERISK\vu.huy.tuan@b120174-pc.sun-asterisk.com>
2021-01-11 13:51:13 +0700

    Create Login page for Fontend

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('admin.dashboard.index');
-});
+Auth::routes();
+
+//require 'admin.php';
+
+Route::view('/', 'welcome');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
